d81ce8b15175e064f562ecef6d2c61c954f839c5
Jeremy Steinberg <jeremy.steinberg@chime.com>
2022-02-04 12:28:27 -0600

    Fix rubocop offenses (#16)

--- spec/support/deser/block_de_ser.rb
+++ spec/support/deser/block_de_ser.rb
@@ -2,7 +2,7 @@
 
 class BlockDeSer < De::Ser::Ializer
   string :string_prop do |object, _context|
-    object.string_prop + '_block'
+    "#{object.string_prop}_block"
   end
   integer :integer_prop do |object, _context|
     object.integer_prop + 1
@@ -10,14 +10,14 @@ class BlockDeSer < De::Ser::Ializer
 
   nested :customer, model_class: OpenStruct do
     string :name do |object, _context|
-      object.name + '_block'
+      "#{object.name}_block"
     end
     string :tele
   end
 
   nested :items, model_class: TestOrderItem do
     string :name do |object, _context|
-      object.name + '_block'
+      "#{object.name}_block"
     end
     integer :quantity
   end
